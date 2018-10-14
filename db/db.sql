53b806e97b31181bda051c4f82c2dd34c8d587f1
Vladislav Klochkov <kvladislavo2013@gmail.com>
2018-10-14 16:07:58 +0300

    Added SQL db initialize script.

---
+++ db/db.sql
@@ -0,0 +1,245 @@
+CREATE DATABASE IF NOT EXISTS `wsdb`;
+USE `wsdb`;
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+DROP TABLE IF EXISTS OptionGroups;
+DROP TABLE IF EXISTS Options;
+DROP TABLE IF EXISTS OrderDetails;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS ProductCategories;
+DROP TABLE IF EXISTS ProductOptions;
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Info;
+
+--
+-- Database: `wsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `OptionGroups`
+--
+
+CREATE TABLE IF NOT EXISTS `OptionGroups` (
+  `OptionGroupID` int(11) NOT NULL AUTO_INCREMENT,
+  `OptionGroupName` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  PRIMARY KEY (`OptionGroupID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `OptionGroups`
+--
+
+INSERT INTO `OptionGroups` (`OptionGroupID`, `OptionGroupName`) VALUES
+(1, 'color'),
+(2, 'size');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Options`
+--
+
+CREATE TABLE IF NOT EXISTS `Options` (
+  `OptionID` int(11) NOT NULL AUTO_INCREMENT,
+  `OptionGroupID` int(11) DEFAULT NULL,
+  `OptionName` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  PRIMARY KEY (`OptionID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `Options`
+--
+
+INSERT INTO `Options` (`OptionID`, `OptionGroupID`, `OptionName`) VALUES
+(1, 1, 'red'),
+(2, 1, 'blue'),
+(3, 1, 'green'),
+(4, 2, 'S'),
+(5, 2, 'M'),
+(6, 2, 'L'),
+(7, 2, 'XL'),
+(8, 2, 'XXL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `OrderDetails`
+--
+
+CREATE TABLE IF NOT EXISTS `OrderDetails` (
+  `DetailID` int(11) NOT NULL AUTO_INCREMENT,
+  `DetailOrderID` int(11) NOT NULL,
+  `DetailProductID` int(11) NOT NULL,
+  `DetailName` varchar(250) COLLATE latin1_german2_ci NOT NULL,
+  `DetailPrice` float NOT NULL,
+  `DetailSKU` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `DetailQuantity` int(11) NOT NULL,
+  PRIMARY KEY (`DetailID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Orders`
+--
+
+CREATE TABLE IF NOT EXISTS `Orders` (
+  `OrderID` int(11) NOT NULL AUTO_INCREMENT,
+  `OrderUserID` int(11) NOT NULL,
+  `OrderAmount` float NOT NULL,
+  `OrderShipName` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `OrderShipAddress` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `OrderShipAddress2` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `OrderCity` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `OrderState` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `OrderZip` varchar(20) COLLATE latin1_german2_ci NOT NULL,
+  `OrderCountry` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `OrderPhone` varchar(20) COLLATE latin1_german2_ci NOT NULL,
+  `OrderFax` varchar(20) COLLATE latin1_german2_ci NOT NULL,
+  `OrderShipping` float NOT NULL,
+  `OrderTax` float NOT NULL,
+  `OrderEmail` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `OrderDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `OrderShipped` tinyint(1) NOT NULL DEFAULT '0',
+  `OrderTrackingNumber` varchar(80) COLLATE latin1_german2_ci DEFAULT NULL,
+  PRIMARY KEY (`OrderID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ProductCategories`
+--
+
+CREATE TABLE IF NOT EXISTS `ProductCategories` (
+  `CategoryID` int(11) NOT NULL AUTO_INCREMENT,
+  `CategoryName` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  PRIMARY KEY (`CategoryID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `ProductCategories`
+--
+
+INSERT INTO `ProductCategories` (`CategoryID`, `CategoryName`) VALUES
+(1, 'Running'),
+(2, 'Walking'),
+(3, 'HIking'),
+(4, 'Track and Trail'),
+(5, 'Short Sleave'),
+(6, 'Long Sleave');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ProductOptions`
+--
+
+CREATE TABLE IF NOT EXISTS `ProductOptions` (
+  `ProductOptionID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ProductID` int(10) unsigned NOT NULL,
+  `OptionID` int(10) unsigned NOT NULL,
+  `OptionPriceIncrement` double DEFAULT NULL,
+  `OptionGroupID` int(11) NOT NULL,
+  PRIMARY KEY (`ProductOptionID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `ProductOptions`
+--
+
+INSERT INTO `ProductOptions` (`ProductOptionID`, `ProductID`, `OptionID`, `OptionPriceIncrement`, `OptionGroupID`) VALUES
+(1, 1, 1, 0, 1),
+(2, 1, 2, 0, 1),
+(3, 1, 3, 0, 1),
+(4, 1, 4, 0, 2),
+(5, 1, 5, 0, 2),
+(6, 1, 6, 0, 2),
+(7, 1, 7, 2, 2),
+(8, 1, 8, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Products`
+--
+
+CREATE TABLE IF NOT EXISTS `Products` (
+  `ProductID` int(12) NOT NULL AUTO_INCREMENT,
+  `ProductSKU` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `ProductName` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `ProductPrice` float NOT NULL,
+  `ProductWeight` float NOT NULL,
+  `ProductCartDesc` varchar(250) COLLATE latin1_german2_ci NOT NULL,
+  `ProductShortDesc` varchar(1000) COLLATE latin1_german2_ci NOT NULL,
+  `ProductLongDesc` text COLLATE latin1_german2_ci NOT NULL,
+  `ProductThumb` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `ProductImage` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `ProductCategoryID` int(11) DEFAULT NULL,
+  `ProductUpdateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ProductStock` float DEFAULT NULL,
+  `ProductLive` tinyint(1) DEFAULT '0',
+  `ProductUnlimited` tinyint(1) DEFAULT '1',
+  `ProductLocation` varchar(250) COLLATE latin1_german2_ci DEFAULT NULL,
+  PRIMARY KEY (`ProductID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=991 ;
+
+--
+-- Dumping data for table `Products`
+--
+
+INSERT INTO `Products` (`ProductID`, `ProductSKU`, `ProductName`, `ProductPrice`, `ProductWeight`, `ProductCartDesc`, `ProductShortDesc`, `ProductLongDesc`, `ProductThumb`, `ProductImage`, `ProductCategoryID`, `ProductUpdateDate`, `ProductStock`, `ProductLive`, `ProductUnlimited`, `ProductLocation`) VALUES
+(1, '000-0001', 'Cotton T-Shirt', 9.99, 3, 'Light Cotton T-Shirt', 'A light cotton T-Shirt made with 100% real cotton.', 'A light cotton T-Shirt made with 100% real cotton.\r\n\r\nMade right here in the USA for over 15 years, this t-shirt is lightweight and durable.', '', '', 5, '2013-06-13 01:00:50', 100, 1, 0, NULL),
+(2, '000-0004', 'Los Angeles', 179.99, 8, 'Track and Trail', 'A rugged track and trail athletic shoe', 'A rugged track and trail athletic shoe', '', '', 4, '2013-07-25 19:04:36', NULL, 0, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `UserID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserEmail` varchar(500) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserPassword` varchar(500) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserFirstName` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserLastName` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserCity` varchar(90) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserState` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserZip` varchar(12) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserEmailVerified` tinyint(1) DEFAULT '0',
+  `UserRegistrationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UserVerificationCode` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserIP` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserPhone` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserFax` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserCountry` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserAddress` varchar(100) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserAddress2` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Info`
+--
+
+CREATE TABLE IF NOT EXISTS `Info` (
+  shopName varchar(255) NOT NULL,
+  foundationDate timestamp NOT NULL,
+  author varchar(255) NOT NULL,
+  about TEXT NOT NULL,
+  primary key (`shopName`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+--
+-- Dumping data for table `Info`
+--
+INSERT INTO `Info` (`shopName`, `foundationDate`, `author`, `about`) VALUES
+('Sell me good', '2018-09-09', 'Vladislav Klochkov', 'This is an e-commerce store');
