8c64a016e8f287250d5247f425b93cecb6f43100
Vladislav Klochkov <kvladislavo2013@gmail.com>
2018-10-21 11:13:45 +0300

    Added HTTPs certificates to client server.

--- client/server.js
+++ client/server.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const https = require('https');
 const http = require('http');
+const fs = require('fs');
+
+const key = fs.readFileSync(path.join(__dirname, '/../encryption/vklochkov.key'));
+const cert = fs.readFileSync(path.join(__dirname, '/../encryption/vklochkov.crt' ));
 
 const app = express();
 
+const options = { key, cert };
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-console.log(__dirname);
 app.use(express.static(path.join(__dirname, 'dist/SellMeGood')));
 
-
+app.use(function(req, res, next) {
+    if (req.secure) {
+        next();
+    } else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/SellMeGood/index.html'));
 });
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '80';
 app.set('port', port);
 
-const server = http.createServer(app);
-
-server.listen(port, () => console.log(`API is running on localhost:${port}`));
+https.createServer(options, app).listen(443);
+http.createServer(app).listen(port, () => console.log(`API is running on localhost:${port}`));
--- client/src/app/component/home/home.component.ts
+++ client/src/app/component/home/home.component.ts
@@ -9,14 +9,14 @@ import { Product } from "../../model/Product";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  private _productList: Product[] = [];
+  public productList: Product[] = [];
 
   constructor(private _productCatalogService: ProductCatalogService, private _productService: ProductService) { }
 
   ngOnInit() {
     this._productService.getProducts().subscribe(
       data => {
-        this._productList = data;
+        this.productList = data;
       },
       error => {
         console.log(error);
